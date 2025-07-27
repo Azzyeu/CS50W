9fd274417a908d2274bc9d42caeed7154b475bdb
Azzyeu <azam.b12354@gmail.com>
2025-07-27 13:55:11 +0100

    MODIFY URL path for search() view

--- Project 1 - Wiki/encyclopedia/urls.py
+++ Project 1 - Wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>/", views.load_page, name="load_page")
+    path("wiki/<str:title>/", views.load_page, name="load_page"),
+    path("search/", views.search, name="search")
 ]

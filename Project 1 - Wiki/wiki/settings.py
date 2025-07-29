21169cc4ce236b01806e3aee8f257de81d3edf50
Azzyeu <azam.b12354@gmail.com>
2025-07-29 23:34:07 +0100

    ADD crispy-django-forms, crispy-bootstrap5 to dependencies and added their config

--- Project 1 - Wiki/wiki/settings.py
+++ Project 1 - Wiki/wiki/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'encyclopedia',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CRISPY
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"

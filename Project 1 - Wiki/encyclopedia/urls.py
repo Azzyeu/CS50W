bb7d8b344933a889044682d1ab4e6c7e9447a6e1
Azzyeu <azam.b12354@gmail.com>
2025-07-29 23:34:35 +0100

    ADD URL for create() view

--- Project 1 - Wiki/encyclopedia/urls.py
+++ Project 1 - Wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/", views.load_page, name="load_page"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create")
 ]

7c68a20787ce8c58aa7b3c0cffc857804919a932
Azzyeu <azam.b12354@gmail.com>
2025-07-31 23:48:23 +0100

    ADD URL to random() view

--- Project 1 - Wiki/encyclopedia/urls.py
+++ Project 1 - Wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("wiki/<str:title>/", views.load_page, name="load_page"),
     path("search/", views.search, name="search"),
     path("create/", views.create, name="create"),
-    path("wiki/<str:title>/edit", views.edit, name="edit")
+    path("wiki/<str:title>/edit", views.edit, name="edit"),
+    path("random/", views.random, name="random")
 ]
